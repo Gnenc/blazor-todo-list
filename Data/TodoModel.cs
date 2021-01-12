d468772c99bc4693597e987d78df042b400e49cc
Gonenc Filiz <gonenc@dakikyazilim.com>
2021-01-12 11:29:27 +0300

    Added new todo table page with MatBlazor components.

---
+++ Data/TodoModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Data
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string Todo { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
